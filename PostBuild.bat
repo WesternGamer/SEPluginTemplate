d64b54c39507618e3f752b28680208b391be9008
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2021-07-12 18:34:11 -0400

    Update PostBuild.bat

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,4 +1,4 @@
-move [Project Location] "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\Plugins"
+move "[Project Location]\bin\Release\[Project Name].dll" "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\Plugins"
 C:
 cd "C:\Program Files (x86)\Steam"
 steam steam://rungameid/244850
