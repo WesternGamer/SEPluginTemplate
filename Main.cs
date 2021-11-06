864405caf9c1214ffba7bde4b916cb3eb51d2308
WesternGamer <kevnguyen474@gmail.com>
2021-11-06 14:16:46 -0400

    Updated documentation.

--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@ namespace SEPluginTemplate
     public class Main : IPlugin
     {
         /// <summary>
-        /// Optional, is not required.
+        /// Constructor, optional, is not required.
         /// </summary>
         public Main()
         {
@@ -15,7 +15,7 @@ namespace SEPluginTemplate
         }
 
         /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
+        /// Called when the game closes. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
         /// </summary>
         public void Dispose()
         {
@@ -23,12 +23,11 @@ namespace SEPluginTemplate
         }
 
         /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
+        /// Called on startup. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
         /// </summary>
         public void Init(object gameInstance)
         {
             //Use the code below in this method if you are planning on using Harmony to modify existing game methods. Optional, is not required. For more info about how harmony works, go to https://harmony.pardeike.net/
-
             // Starts an instance of Harmony
             Harmony harmony = new Harmony("SEPluginTemplate");
             // Patches all patches in the plugin.
@@ -36,7 +35,7 @@ namespace SEPluginTemplate
         }
 
         /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
+        /// Called every game update. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
         /// </summary>
         public void Update()
         {
